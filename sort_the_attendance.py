06dba077ae0c226dd42afeffadd14f7216103837
haydencordeio <cordeirohayden@gmail.com>
2020-04-08 23:50:57 +0530

    Gui made

--- sort_the_attendance.py
+++
@@ -1,8 +0,0 @@
-import os
-lines_seen = set() # holds lines already seen
-outfile = open("sorted attendance.txt", "w")
-for line in open("att.txt", "r"):
-    if line not in lines_seen: # not a duplicate
-        outfile.write(line)
-        lines_seen.add(line)
-outfile.close()
